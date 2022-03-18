cf75b1ff2872ae7e9ed1813bdaa8905039af1a2b
Helen Tsui <helentsui.tht@gmail.com>
2022-03-18 23:33:36 +0800

    Use Flask as backend to scrape iHerb website

---
+++ backend/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_cors import CORS
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/iherbbarcode/<barcode_num>")
+def iherb_web_scrapper(barcode_num):
+    URL = "https://hk.iherb.com/search?kw="+str(barcode_num)
+    r = requests.get(URL)
+
+    soup = BeautifulSoup(r.content, "html.parser")
+    product_title = soup.find("div", class_="product-title")
+    product_price = soup.find("div", class_="product-price-top")
+
+    if product_title is not None:
+
+        product_title = product_title.text.strip()
+        product_price = product_price.text.strip().partition('\n')[0]
+        results = {product_title:product_price}
+        print(results)
+        return results
+
+    else:
+        print("Not found")
+        return None
