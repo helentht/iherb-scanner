2dc24c0c6c94c77eedd2a892283e1bca9796e415
Helen Tsui <helentsui.tht@gmail.com>
2022-03-19 23:05:46 +0800

    Refactor to put components in Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import BarcodeScannerComponent from "react-qr-barcode-scanner";
-import axios from "axios";
+
+import Home from "./Pages/Home";
 
 function App() {
-  const [data, setData] = useState<String>();
-
-  useEffect(() => {
-    if (data) {
-      axios
-        .get(`http://127.0.0.1:5000/iherbbarcode/${data}`)
-        .then((response) => {
-          console.log(response.data);
-        })
-        .catch((error) => console.error(error));
-    }
-  }, [data]);
-
   return (
     <div className="App">
       <header className="App-header">
-        {data && (
-          <iframe
-            src={`https://hk.iherb.com/search?kw=${data}`}
-            title="iherb-iframe"
-            width="100%"
-            height={500}
-          />
-        )}
-        {!data && (
-          <BarcodeScannerComponent
-            width={250}
-            height={250}
-            onUpdate={(err, result) => {
-              if (result) {
-                console.log(result.getText().slice(1));
-                setData(result.getText().slice(1));
-              } else setData("898220010653");
-            }}
-          />
-        )}
+        <Home />
       </header>
     </div>
   );
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,45 @@
+import React, { useEffect, useState } from "react";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import axios from "axios";
+
 function Home() {
-  return <></>;
+  const [data, setData] = useState<String>();
+
+  useEffect(() => {
+    if (data) {
+      axios
+        .get(`http://127.0.0.1:5000/iherbbarcode/${data}`)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => console.error(error));
+    }
+  }, [data]);
+
+  return (
+    <>
+      {data && (
+        <iframe
+          src={`https://hk.iherb.com/search?kw=${data}`}
+          title="iherb-iframe"
+          width="100%"
+          height={500}
+        />
+      )}
+      {!data && (
+        <BarcodeScannerComponent
+          width={250}
+          height={250}
+          onUpdate={(err, result) => {
+            if (result) {
+              console.log(result.getText().slice(1));
+              setData(result.getText().slice(1));
+            } else setData("898220010653");
+          }}
+        />
+      )}
+    </>
+  );
 }
 
 export default Home;
