906e82e859c64435588c617cf3ac54ca36849dda
Helen Tsui <helentsui.tht@gmail.com>
2022-03-25 23:35:24 +0800

    Add Spin when waiting for backend response

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
 import axios from "axios";
-import { Button, Typography } from "antd";
+import { Button, Typography, Spin } from "antd";
 
 const { Title } = Typography;
 
@@ -9,6 +9,7 @@ function Home() {
   const [data, setData] = useState<String>();
   const [name, setName] = useState<String>();
   const [price, setPrice] = useState<String>();
+  const [isSpinning, setIsSpinning] = useState<boolean>(false);
 
   useEffect(() => {
     if (data) {
@@ -19,6 +20,7 @@ function Home() {
           const name = Object.keys(obj)[0];
           setName(name);
           setPrice(obj[name]);
+          setIsSpinning(false);
           localStorage.setItem("name", name);
           localStorage.setItem("price", obj[name]);
           localStorage.setItem("dateAdded", new Date().toDateString());
@@ -44,6 +46,7 @@ function Home() {
           <Title level={3}>{price}</Title>
         </>
       )}
+      <Spin spinning={isSpinning} />
       {!data && (
         <BarcodeScannerComponent
           width={250}
@@ -52,6 +55,7 @@ function Home() {
             if (result) {
               console.log(result.getText().slice(1));
               setData(result.getText().slice(1));
+              setIsSpinning(true);
             } else setData(undefined);
           }}
         />
